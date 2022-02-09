b85489e5a3c6b2fb687c1338c2f979ad2315ccb8
chavdar00 <chavdargoranov00@gmail.com>
2022-02-09 21:23:11 +0200

    update ER Table

--- SQL_dominos.sql
+++ SQL_dominos.sql
@@ -7,17 +7,20 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- -----------------------------------------------------
 -- Schema mydb
 -- -----------------------------------------------------
+-- -----------------------------------------------------
+-- Schema dominos
+-- -----------------------------------------------------
 
 -- -----------------------------------------------------
--- Schema mydb
+-- Schema dominos
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
-USE `mydb` ;
+CREATE SCHEMA IF NOT EXISTS `dominos` DEFAULT CHARACTER SET utf8 ;
+USE `dominos` ;
 
 -- -----------------------------------------------------
--- Table `mydb`.`users`
+-- Table `dominos`.`users`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`users` (
+CREATE TABLE IF NOT EXISTS `dominos`.`users` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `first_name` VARCHAR(45) NOT NULL,
   `last_name` VARCHAR(45) NOT NULL,
@@ -25,13 +28,14 @@ CREATE TABLE IF NOT EXISTS `mydb`.`users` (
   `password` VARCHAR(150) NOT NULL,
   `phone_number` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`id`))
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`addresses`
+-- Table `dominos`.`addresses`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`addresses` (
+CREATE TABLE IF NOT EXISTS `dominos`.`addresses` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `user_id` INT NOT NULL,
   `address_name` VARCHAR(150) NOT NULL,
@@ -39,101 +43,115 @@ CREATE TABLE IF NOT EXISTS `mydb`.`addresses` (
   INDEX `users_addresses_fk_idx` (`user_id` ASC) VISIBLE,
   CONSTRAINT `users_addresses_fk`
     FOREIGN KEY (`user_id`)
-    REFERENCES `mydb`.`users` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+    REFERENCES `dominos`.`users` (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`orders`
+-- Table `dominos`.`ingredients`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`orders` (
+CREATE TABLE IF NOT EXISTS `dominos`.`ingredients` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  `price` DECIMAL(15,2) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
+
+
+-- -----------------------------------------------------
+-- Table `dominos`.`orders`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `dominos`.`orders` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `user_id` INT NOT NULL,
   `ordered_at` DATE NOT NULL,
   `address_id` INT NOT NULL,
-  `instructions` VARCHAR(150) NULL,
+  `instructions` VARCHAR(150) NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
   INDEX `orders_users_fk_idx` (`user_id` ASC) VISIBLE,
   INDEX `orders_addresses_fk_idx` (`address_id` ASC) VISIBLE,
-  CONSTRAINT `orders_users_fk`
-    FOREIGN KEY (`user_id`)
-    REFERENCES `mydb`.`users` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
   CONSTRAINT `orders_addresses_fk`
     FOREIGN KEY (`address_id`)
-    REFERENCES `mydb`.`addresses` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+    REFERENCES `dominos`.`addresses` (`id`),
+  CONSTRAINT `orders_users_fk`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `dominos`.`users` (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`pizzas`
+-- Table `dominos`.`other_products`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`pizzas` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(150) NOT NULL,
-  `price` INT NOT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `mydb`.`ingredients`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`ingredients` (
+CREATE TABLE IF NOT EXISTS `dominos`.`other_products` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) NOT NULL,
-  `price` INT NOT NULL,
+  `price` DECIMAL(15,2) NOT NULL,
+  `image` VARCHAR(150) NOT NULL,
   PRIMARY KEY (`id`))
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`other_products`
+-- Table `dominos`.`orders_have_other_products`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`other_products` (
+CREATE TABLE IF NOT EXISTS `dominos`.`orders_have_other_products` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) NOT NULL,
-  `price` INT NOT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
+  `order_id` INT NOT NULL,
+  `other_product_id` INT NOT NULL,
+  `amount` INT NOT NULL,
+  `price` DECIMAL(15,2) NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `order_to_other_products_fk_idx` (`order_id` ASC) VISIBLE,
+  INDEX `other_products_fk_idx` (`other_product_id` ASC) VISIBLE,
+  CONSTRAINT `order_to_other_products_fk`
+    FOREIGN KEY (`order_id`)
+    REFERENCES `dominos`.`orders` (`id`),
+  CONSTRAINT `other_products_fk`
+    FOREIGN KEY (`other_product_id`)
+    REFERENCES `dominos`.`other_products` (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`pizza_sizes`
+-- Table `dominos`.`pizza_breads`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`pizza_sizes` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `size` INT NOT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `mydb`.`pizza_breads`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`pizza_breads` (
+CREATE TABLE IF NOT EXISTS `dominos`.`pizza_breads` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `bread_type` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`id`))
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`orders_have_pizzas`
+-- Table `dominos`.`pizzas`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`orders_have_pizzas` (
+CREATE TABLE IF NOT EXISTS `dominos`.`pizzas` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(150) NOT NULL,
+  `price` DECIMAL(15,2) NOT NULL,
+  `image` VARCHAR(150) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
+
+
+-- -----------------------------------------------------
+-- Table `dominos`.`orders_have_pizzas`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `dominos`.`orders_have_pizzas` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `order_id` INT NOT NULL,
   `pizza_id` INT NOT NULL,
   `amount` INT NOT NULL,
-  `price` INT NOT NULL,
+  `price` DECIMAL(15,2) NOT NULL,
   `pizza_breads_id` INT NOT NULL,
-  `pizza_sizes_id` INT NOT NULL,
+  `pizza_sizes_id` DECIMAL(15,2) NOT NULL,
   PRIMARY KEY (`id`),
   INDEX `orders_pizzas_fk_idx` (`order_id` ASC) VISIBLE,
   INDEX `pizzas_fk_idx` (`pizza_id` ASC) VISIBLE,
@@ -141,71 +159,44 @@ CREATE TABLE IF NOT EXISTS `mydb`.`orders_have_pizzas` (
   INDEX `pizza_sizes_fk_idx` (`pizza_sizes_id` ASC) VISIBLE,
   CONSTRAINT `orders_fk`
     FOREIGN KEY (`order_id`)
-    REFERENCES `mydb`.`orders` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `pizzas_fk`
-    FOREIGN KEY (`pizza_id`)
-    REFERENCES `mydb`.`pizzas` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
+    REFERENCES `dominos`.`orders` (`id`),
   CONSTRAINT `pizza_breads_fk`
     FOREIGN KEY (`pizza_breads_id`)
-    REFERENCES `mydb`.`pizza_breads` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `pizza_sizes_fk`
-    FOREIGN KEY (`pizza_sizes_id`)
-    REFERENCES `mydb`.`pizza_sizes` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+    REFERENCES `dominos`.`pizza_breads` (`id`),
+  CONSTRAINT `pizzas_fk`
+    FOREIGN KEY (`pizza_id`)
+    REFERENCES `dominos`.`pizzas` (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`orders_have_other_products`
+-- Table `dominos`.`pizza_sizes`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`orders_have_other_products` (
+CREATE TABLE IF NOT EXISTS `dominos`.`pizza_sizes` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `order_id` INT NOT NULL,
-  `other_product_id` INT NOT NULL,
-  `amount` INT NOT NULL,
-  `price` INT NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `order_to_other_products_fk_idx` (`order_id` ASC) VISIBLE,
-  INDEX `other_products_fk_idx` (`other_product_id` ASC) VISIBLE,
-  CONSTRAINT `order_to_other_products_fk`
-    FOREIGN KEY (`order_id`)
-    REFERENCES `mydb`.`orders` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `other_products_fk`
-    FOREIGN KEY (`other_product_id`)
-    REFERENCES `mydb`.`other_products` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+  `size` INT NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`pizzas_have_ingredients`
+-- Table `dominos`.`pizzas_have_ingredients`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`pizzas_have_ingredients` (
+CREATE TABLE IF NOT EXISTS `dominos`.`pizzas_have_ingredients` (
   `pizza_id` INT NOT NULL,
   `ingredient_id` INT NOT NULL,
   INDEX `pizzas_have_ingredients_pizza_fk_idx` (`pizza_id` ASC) VISIBLE,
   INDEX `pizzas_have_ingredients_ingredient_fk_idx` (`ingredient_id` ASC) VISIBLE,
-  CONSTRAINT `pizzas_have_ingredients_pizza_fk`
-    FOREIGN KEY (`pizza_id`)
-    REFERENCES `mydb`.`pizzas` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
   CONSTRAINT `pizzas_have_ingredients_ingredient_fk`
     FOREIGN KEY (`ingredient_id`)
-    REFERENCES `mydb`.`ingredients` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+    REFERENCES `dominos`.`ingredients` (`id`),
+  CONSTRAINT `pizzas_have_ingredients_pizza_fk`
+    FOREIGN KEY (`pizza_id`)
+    REFERENCES `dominos`.`pizzas` (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
